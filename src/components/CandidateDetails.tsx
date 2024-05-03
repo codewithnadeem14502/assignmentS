b28bc3825d5d185cd0dcfda8a0c019567ffbb905
codewithnadeem14502 <codewithnadeem@14502>
2024-05-03 19:39:04 +0530

    Completed

--- src/components/CandidateDetails.tsx
+++ src/components/CandidateDetails.tsx
@@ -2,6 +2,7 @@
 import { useState } from "react";
 import { Candidate } from "../utils/interfaces";
 import VideoCarousel from "./VideoCarousel";
+import Image from "next/image";
 
 interface UserDetailsProps {
   Candidate: Candidate | null;
@@ -57,7 +58,7 @@ const CandidateDetails: React.FC<UserDetailsProps> = ({ Candidate }) => {
       <div className="w-[100%] h-auto  ">
         <div className="flex justify-between items-center ">
           <div className="flex gap-2 items-center">
-            <img
+            <Image
               src={Candidate.profileImg}
               alt={Candidate.full_name}
               className="w-20 h-20 rounded-lg "
